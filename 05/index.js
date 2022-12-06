7d54247fd39c4c88588ec5ce6181d5f5a5002722
Erik Withak <erik@goldenk.tech>
2022-12-05 16:45:01 -0800

    day 5

---
+++ 05/index.js
@@ -0,0 +1,48 @@
+// Part 1
+const input = require("./input");
+const { 0: base, 2: moves } = input.split(/([\s\d]*\n\n)/);
+const parsedBase = base
+  .replace(/[\[\]]/g, " ")
+  .split("\n")
+  .filter(Boolean)
+  .map((row) => row.match(/.{1,4}/g).map((letter) => letter.trim()))
+  .reduce((all, current) => {
+    current.forEach((letter, index) => {
+      if (letter === "") return;
+      all[index + 1]
+        ? all[index + 1].push(letter)
+        : (all[index + 1] = [letter]);
+    });
+    return all;
+  }, {});
+
+const parsedMoves = moves.split("\n").reduce((all, current) => {
+  const { 1: size, 3: fromStack, 5: toStack } = current.split(" ");
+  all.push({ size, fromStack, toStack });
+  return all;
+}, []);
+
+console.log(parsedMoves[parsedMoves.length - 1]);
+
+const finalStacks = parsedMoves.reduce((all, { size, fromStack, toStack }) => {
+  all[toStack].unshift(...all[fromStack].splice(0, size).reverse());
+  return all;
+}, JSON.parse(JSON.stringify(parsedBase)));
+
+const answer = Object.values(finalStacks)
+  .map((value) => value[0])
+  .join("");
+
+console.log(answer);
+
+// Part 2
+const finalStacks2 = parsedMoves.reduce((all, { size, fromStack, toStack }) => {
+  all[toStack].unshift(...all[fromStack].splice(0, size));
+  return all;
+}, JSON.parse(JSON.stringify(parsedBase)));
+
+const answer2 = Object.values(finalStacks2)
+  .map((value) => value[0])
+  .join("");
+
+console.log(answer2);
