ce996a5f6d90a070f651b1ac63e6365cebc80d3f
Erik Withak <erik@goldenk.tech>
2022-12-07 19:46:01 -0800

    cleans up

--- 07/index.js
+++ 07/index.js
@@ -2,14 +2,15 @@
 
 const input = require("./input.js");
 const MAX_SIZE = 100_000;
-
 const structure = {};
 
 const calculateFolderStuff = ({ path, linesLeft }) => {
   if (!linesLeft.length) {
     return;
   }
+
   const [line] = linesLeft.splice(0, 1);
+
   if (line.match(/\d+/)) {
     // We have file size info
     const fileSize = Number(line.match(/\d+/)[0]);
@@ -24,10 +25,7 @@ const calculateFolderStuff = ({ path, linesLeft }) => {
       path,
     });
   }
-  if (line.match(/dir/) || line.match(/\$\sls/)) {
-    // noop
-    return calculateFolderStuff({ linesLeft, path });
-  }
+
   if (line.match(/\$\scd\s\.\./)) {
     // Go up a directory
     const directories = path.split("/");
@@ -35,6 +33,7 @@ const calculateFolderStuff = ({ path, linesLeft }) => {
     const newPath = directories.join("/");
     return calculateFolderStuff({ path: newPath, linesLeft });
   }
+
   if (line.match(/\$\scd\s\w*/)) {
     // Navigate to a directory
     const directory = line.split(" ")[2];
@@ -47,16 +46,16 @@ const calculateFolderStuff = ({ path, linesLeft }) => {
       linesLeft,
     });
   }
+
+  // This is the fallback for lines we don't care about ("dir foo" and "$ ls")
+  return calculateFolderStuff({ linesLeft, path });
 };
 
 calculateFolderStuff({ linesLeft: input.split("\n") });
 console.log(structure);
 
 const answer = Object.values(structure).reduce((total, size) => {
-  if (size <= MAX_SIZE) {
-    total += size;
-  }
-  return total;
+  return size <= MAX_SIZE ? total + size : total;
 }, 0);
 
 console.log(answer); // 1538808 = incorrect
